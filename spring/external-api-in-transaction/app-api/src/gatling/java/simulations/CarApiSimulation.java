4930d6b7ae9c81ee2a12aca210051acb66df0227
khc <nightletter65@gmail.com>
2026-03-05 04:23:43 +0900

    게틀링 코드 수정

--- spring/external-api-in-transaction/app-api/src/gatling/java/simulations/CarApiSimulation.java
+++ spring/external-api-in-transaction/app-api/src/gatling/java/simulations/CarApiSimulation.java
@@ -4,6 +4,7 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 import static java.time.Duration.ofSeconds;
 
+import io.gatling.core.session.package$;
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
@@ -17,23 +18,25 @@ public class CarApiSimulation extends Simulation {
 
     ScenarioBuilder scn = scenario("Car Registration")
             .exec(http("Create Car Task")
-                    .post("/v4/car")
-                    .check(jsonPath("$.taskId").saveAs("taskId"))
+//                    .post("/v3/car")
+                            .post("/v4/car")
+                            .check(jsonPath("$.taskId").saveAs("taskId"))
             )
             .exec(session -> session.set("status", "PENDING")) // 강제로 세션에 상태 주입, 없으면 종료됨
             .pause(ofSeconds(2))
             .asLongAs(session -> !session.getString("status").equals("SUCCESS"))
             .on(
                     exec(http("Check Task Status")
-                            .get(session -> "/v4/car/" + session.getString("taskId") + "/status")
-                            .check(jsonPath("$.status").saveAs("status"))
+//                            .get(session -> "/v3/car/" + session.getString("taskId") + "/status")
+                                    .get(session -> "/v4/car/" + session.getString("taskId") + "/status")
+                                    .check(jsonPath("$.status").saveAs("status"))
                     )
                             .pause(ofSeconds(5)) // 5초 대기후 루프, 풀링 흉내
             );
 
     {
         setUp(
-                scn.injectOpen(atOnceUsers(50))
+                scn.injectOpen(atOnceUsers(200))
         ).protocols(httpProtocol);
     }
 }
