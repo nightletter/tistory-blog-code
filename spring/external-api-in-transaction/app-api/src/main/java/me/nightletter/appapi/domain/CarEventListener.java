c6c804943aab70a0e3c55403929997ddd457ddd8
khc <nightletter65@gmail.com>
2026-03-05 03:16:36 +0900

    코드수정

--- spring/external-api-in-transaction/app-api/src/main/java/me/nightletter/appapi/domain/CarEventListener.java
+++ spring/external-api-in-transaction/app-api/src/main/java/me/nightletter/appapi/domain/CarEventListener.java
@@ -2,6 +2,7 @@ package me.nightletter.appapi.domain;
 
 import lombok.RequiredArgsConstructor;
 import me.nightletter.appapi.messaging.CarRegistrationMessage;
+import me.nightletter.appapi.messaging.RedisKeys;
 import me.nightletter.appapi.messaging.RedisTopics;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -15,11 +16,10 @@ import static java.time.Duration.ofMinutes;
 public class CarEventListener {
 
     private final RedisTemplate<String, Object> redisTemplate;
-    private String KEY_PREFIX = "task:";
 
     @EventListener
     public void onEvent(CarRegistrationEvent event) {
-        redisTemplate.opsForValue().set(KEY_PREFIX + event.getTaskId(), "PENDING", ofMinutes(10));
+        redisTemplate.opsForValue().set(RedisKeys.TASK.toKey(event.getTaskId()), "PENDING", ofMinutes(10));
         redisTemplate.convertAndSend(
                 RedisTopics.CAR_REGISTRATION,
                 new CarRegistrationMessage(event.getTaskId(), event.getOwner(), event.getLicensePlate())
